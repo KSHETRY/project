db65ac41872dac1cd347ff82b6ba75da498669f2
kshetrydc <chhetrydc@gmail.com>
2019-08-13 01:03:52 +0545

    comitting a text file

---
+++ AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Co-Operative")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Channakya Software Pvt. Ltd.")> 
+<Assembly: AssemblyProduct("Co-Operative")> 
+<Assembly: AssemblyCopyright("Copyright © Channakya Softwarel 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6d6a9c82-d5de-455c-9b71-dd0281b387b4")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("20.74.01.11")> 
+<Assembly: AssemblyFileVersion("20.74.01.11")> 
